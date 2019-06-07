fceeadb0473df85688642424007dc0954f7c36b3
alexander.vieira <alexander.vieira@RJ10TEC002>
2019-06-07 14:36:59 -0300

    Add project files.

---
+++ AspNetMvcWithSQLserver/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvcWithSQLserver.Startup))]
+namespace AspNetMvcWithSQLserver
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
